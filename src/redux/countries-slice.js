a3847b62062c0e480f14d17e909e308bf6bcbd6c
Mohammed roshdy <xmedox20111@gmail.com>
2023-09-06 19:10:51 +0300

    Fixed linters

--- src/redux/countries-slice.js
+++ src/redux/countries-slice.js
@@ -1,14 +1,44 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getCountries = createAsyncThunk('countries/getCountries', async () => {
-    const response = await fetch('https://restcountries.com/v3.1/all');
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
 
-    const filteredData = data.filter((country) => country.name.common !== 'Israel');
+  const filteredData = data.filter((country) => country.name.common !== 'Israel');
 
+  const EgyptData = {
+    flags: {
+      png: 'https://img.freepik.com/premium-vector/3d-realistic-pennant-with-flag_97886-2626.jpg',
+    },
+    name: {
+      common: 'Egypt',
+      official: 'State of Egypt',
+    },
+    region: 'Africa',
+    capital: ['Cairo'],
+    area: 1000.000,
+    population: 105052776,
+    timezones: ['UTC+02:00'],
+    currencies: {
+      ILS: {
+        name: 'Egyptian Pound',
+        symbol: 'EG',
+      },
+    },
+    languages: {
+      ara: 'Arabic',
+    },
+  };
+  return [EgyptData, ...filteredData];
+});
+
+export const getCountry = createAsyncThunk('countries/getCountry', async (name) => {
+  if (name === 'Israel') {
+    return null;
+  } if (name === 'Egypt') {
     const EgyptData = {
       flags: {
         png: 'https://img.freepik.com/premium-vector/3d-realistic-pennant-with-flag_97886-2626.jpg',
@@ -32,45 +62,15 @@ export const getCountries = createAsyncThunk('countries/getCountries', async () 
         ara: 'Arabic',
       },
     };
-    return [EgyptData, ...filteredData];
-});
+    return EgyptData;
+  }
 
-export const getCountry = createAsyncThunk('countries/getCountry', async (name) => {
-    if (name === 'Israel') {
-      return null;
-    } if (name === 'Egypt') {
-      const EgyptData = {
-        flags: {
-          png: 'https://img.freepik.com/premium-vector/3d-realistic-pennant-with-flag_97886-2626.jpg',
-        },
-        name: {
-          common: 'Egypt',
-          official: 'State of Egypt',
-        },
-        region: 'Africa',
-        capital: ['Cairo'],
-        area: 1000.000,
-        population: 105052776,
-        timezones: ['UTC+02:00'],
-        currencies: {
-          ILS: {
-            name: 'Egyptian Pound',
-            symbol: 'EG',
-          },
-        },
-        languages: {
-          ara: 'Arabic',
-        },
-      };
-      return EgyptData;
-    }
-
-    const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data[0];  
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data[0];
 });
 
 const countriesSlice = createSlice({
