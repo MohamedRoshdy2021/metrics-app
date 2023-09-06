73cdf89195776372ea1b094a8d78978169e5c338
Mohammed roshdy <xmedox20111@gmail.com>
2023-09-06 19:09:41 +0300

    Fixed tests for the countrys slice

--- src/__tests__/countrCard.test.js
+++ src/__tests__/countrCard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CountryCard from '../components/Home/CountryCard';
+import CountryCard from '../components/CountryCard';
 
 const mockCountry = {
   name: {
--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from '../components/Header/Header';
+import Header from '../components/Header';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Header', () => {
